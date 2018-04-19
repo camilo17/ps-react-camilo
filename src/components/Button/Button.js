35a9b6758e01a1ece989af7693cba0bb47a6d57f
camilo17 <cami172013@gmail.com>
2018-04-19 16:06:59 -0500

    made a build process and move around some dependencies.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,81 +1,49 @@
-import React from'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
-
-const Btn = styled.button`    
-    padding: .25em 1em;
-    display: inline-block;
-    border-radius: 3px;
-    color: ${props => props.color};
-    background: ${props => props.transparent ? 'transparent' : props.bgColor};
-    border: 2px solid ${props => props.borderColor ? props.borderColor : props.bgColor};
-    text-decoration: none;
-    margin: 0 1em;
-    cursor: pointer;
-
-
-    
-
-
-    
-
-
-
-    &:hover {
-        transform: scale( 1.2, 1.2);
-    }
-`
-
+const Btn = styled.button``;
 
 const Link = styled.a`
-    text-decoration: none;
-    
-    &:link,&:visited {
-        color: ${props => props.color? props.color : 'palevioletred'};
-        
-    }
-`
+  text-decoration: none;
+
+  &:link,
+  &:visited {
+    color: inherit;
+  }
+`;
 
 class Button extends React.Component {
-
-    handleHref() {
-        if(this.props.href){
-            return (
-                <Link target="_blank" href={this.props.href}{...this.props}>{this.props.children}</Link>
-            )
-        } else {
-            return (
-                <Link href="#"{...this.props}>{this.props.children}</Link>
-            )
-            
-        }
+  handleHref = () => {
+    const { children } = this.props;
+    if (this.props.href) {
+      return (
+        <Link target="_blank" href={this.props.href} {...this.props}>
+          {this.props.children}
+        </Link>
+      );
+    } else {
+      return children;
     }
-    render() {
-        return (
-            <Btn {...this.props}>
-                {this.handleHref()}
-            </Btn>    
-        )
-    }
+  };
+  render() {
+    return <Btn {...this.props}>{this.handleHref()}</Btn>;
+  }
 }
-
 
 Button.defaultProps = {
-    color: 'white',
-    bgColor: 'purple',
-    borderColor: 'purple'
-}
-
+  color: "white",
+  bgColor: "purple",
+  borderColor: "purple"
+};
 
 Button.propTypes = {
-    bgColor: PropTypes.string,
-    transparent: PropTypes.bool,
-    href: PropTypes.string,
-    color: PropTypes.string,
-    disabled: PropTypes.bool,
-    borderColor: PropTypes.string
-}
-
+  bgColor: PropTypes.string,
+  transparent: PropTypes.bool,
+  href: PropTypes.string,
+  color: PropTypes.string,
+  disabled: PropTypes.bool,
+  borderColor: PropTypes.string
+};
 
 export default Button;
--- src/docs/examples/Card/CardExample.js
+++ src/docs/examples/Card/CardExample.js
@@ -1,27 +1,19 @@
-import React from 'react';
-import Card from 'react-ultra-instinct/Card';
-import Button from 'react-ultra-instinct/Button';
-
+import React from "react";
+import Card from "react-ultra-instinct/Card";
+import Button from "react-ultra-instinct/Button";
 
 export default () => {
-    const image = 'https://images.pexels.com/photos/880871/pexels-photo-880871.jpeg?w=940&h=650&auto=compress&cs=tinysrgb';
+  const image =
+    "https://images.pexels.com/photos/880871/pexels-photo-880871.jpeg?w=940&h=650&auto=compress&cs=tinysrgb";
 
-    return (
-        
-            <Card
-            bgImage={image}
-            title="React"
-            >
-               Hi
-                <div>
-
-                    <Button
-                     bgColor="turquoise"
-                     borderColor="turquoise"
-                     color="black"
-                    >React Docs</Button>
-                </div>    
-            </Card>
-       
-    )
-}
+  return (
+    <Card bgImage={image} title="React">
+      Hi
+      <div>
+        <Button bgColor="turquoise" borderColor="turquoise" color="black">
+          React Docs
+        </Button>
+      </div>
+    </Card>
+  );
+};
