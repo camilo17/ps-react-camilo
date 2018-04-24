80d978e2008fe949eed5ab8fb53d128d58569d2b
camilo17 <cami172013@gmail.com>
2018-04-24 15:35:54 -0500

    "Made some improvements on the Divider."

--- src/components/Divider/Divider.js
+++ src/components/Divider/Divider.js
@@ -1,19 +1,44 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import is from "styled-is";
+import PropTypes from "prop-types";
 
 const Divi = styled.hr`
+  width: ${props => props.size};
+
   ${is("vertical")`
     
         transform: rotate(90deg);
-        width: 80px;
+        
+    `};
+
+  ${({ color }) =>
+    color &&
+    css`
+      background: color;
     `};
 `;
 
 class Divider extends Component {
   render() {
-    return <Divi vertical />;
+    return <Divi {...this.props} />;
   }
 }
+
+Divider.defaultProps = {
+  vertical: false,
+  size: "auto",
+  color: "#C0C0C0"
+};
+
+Divider.propTypes = {
+  /** Make Divider vertical instead of horizontal */
+  vertical: PropTypes.bool,
+
+  /** Width Divider */
+  size: PropTypes.string,
+  /** Color of the Divider*/
+  color: PropTypes.string
+};
 
 export default Divider;
