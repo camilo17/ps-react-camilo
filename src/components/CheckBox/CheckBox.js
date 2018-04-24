8046f373c9b23d640daf1c1a8d5cf120abc84382
camilo17 <cami172013@gmail.com>
2018-04-24 15:56:59 -0500

    Added more proptypes to Checkbox, will need Icon Component to make Checkbox better.

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
-export default class CheckBox extends Component {
+const Check = styled.input.attrs({
+  type: "checkbox"
+})``;
+class CheckBox extends Component {
   render() {
-    return <div />;
+    return <Check />;
   }
 }
+
+CheckBox.propTypes = {
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  checked: PropTypes.bool.isRequired,
+  id: PropTypes.bool.isRequired,
+  name: PropTypes.string
+};
+
+CheckBox.defaultProps = {
+  size: "medium"
+};
+
+export default CheckBox;
