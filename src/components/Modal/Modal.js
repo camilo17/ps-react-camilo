8f801c494ed3bdfbf22fa9252526615b80cee747
camilo17 <cami172013@gmail.com>
2018-05-02 15:40:20 -0500

    Made some minor changes

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import is from "styled-is";
 
 // TODO ADD TRIGGER PROP TO RENDER
