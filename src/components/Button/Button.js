1219004e05887e70794c3a764111e9490069a851
camilo17 <cami172013@gmail.com>
2018-05-28 16:51:32 -0500

    Made more improvements on the button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,32 +4,43 @@ import styled from "styled-components";
 import { darken } from "polished";
 const Btn = styled.button`
   background: ${props => props.color};
+  border-color: ${props => props.color};
 
   width: 100px;
   height: auto;
   border: none;
   border-radius: 2px;
-  padding: 6px 6px;
+  padding: 7px 7px;
   color: white;
+  font-size: auto;
+  font-weight: 70px;
 
   &:hover {
     background: ${props => darken(0.07)(props.color)};
+    border-color: ${props => darken(0.07)(props.color)};
+    letter-spacing: 1px;
+    cursor: pointer;
   }
 `;
 class Button extends React.Component {
   render() {
-    const { color } = this.props;
-    return <Btn color={color}>{this.props.children}</Btn>;
+    const { color, disabled, onClick } = this.props;
+    return (
+      <Btn disabled={disabled} onClick={onClick} color={color}>
+        {this.props.children}
+      </Btn>
+    );
   }
 }
 
 Button.defaultProps = {
-  color: "purple"
+  color: "orangered"
 };
 
 Button.propTypes = {
   /** Blue | Red | Purple */
-  color: PropTypes.oneOf(["blue", "red", "purple"])
+  color: PropTypes.string,
+  display: PropTypes.bool
 };
 
 export default Button;
