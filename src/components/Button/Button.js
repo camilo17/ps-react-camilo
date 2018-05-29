846eaf527435a5f012ee78e8a5de2fe69de7706f
camilo17 <cami172013@gmail.com>
2018-05-29 18:10:17 -0500

    Added size options and fixed the box shadow

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,31 +2,69 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { darken } from "polished";
+import is from "styled-is";
+
+const btnSizes = {
+  sm: "100px",
+  md: "120px",
+  lg: "250px",
+  xlarge: "325px"
+};
+
 const Btn = styled.button`
   background: ${props => props.color};
   border-color: ${props => props.color};
+ 
 
-  width: 100px;
-  height: auto;
+  width: ${props => btnSizes[props.size]};
+  height: auto; 
   border: none;
   border-radius: 2px;
   padding: 7px 7px;
   color: white;
-  font-size: auto;
-  font-weight: 70px;
+  font-size: 17px;
+  letter-spacing: .5px;
+  font-weight: normal;
+  box-shadow: 5px 5px 7px #808080;  
+  font-family: Arial, Helvetica, sans-serif
+
+  ${is("full")`
+    width: 100%
+    `}
+  
+}
 
   &:hover {
     background: ${props => darken(0.07)(props.color)};
     border-color: ${props => darken(0.07)(props.color)};
-    letter-spacing: 1px;
+    letter-spacing: .5px;
     cursor: pointer;
+  }
+
+
+  &:active {
+    box-shadow: 3px 2px 8px #808080;  
+  }
+
+
+  &:disabled {
+    background: grey;
+    box-shadow: none;
+    letter-spacing: normal;
+    cursor: default;
   }
 `;
 class Button extends React.Component {
   render() {
-    const { color, disabled, onClick } = this.props;
+    const { color, disabled, onClick, full, size } = this.props;
     return (
-      <Btn disabled={disabled} onClick={onClick} color={color}>
+      <Btn
+        full={full}
+        disabled={disabled}
+        size={size}
+        onClick={onClick}
+        color={color}
+      >
         {this.props.children}
       </Btn>
     );
@@ -34,13 +72,15 @@ class Button extends React.Component {
 }
 
 Button.defaultProps = {
-  color: "orangered"
+  color: "#8642f4",
+  size: "md"
 };
 
 Button.propTypes = {
-  /** Blue | Red | Purple */
   color: PropTypes.string,
-  display: PropTypes.bool
+  disabled: PropTypes.bool,
+  type: PropTypes.oneOf(["submit, button"]),
+  size: PropTypes.oneOf(["sm", "md", "lg", "xlg"])
 };
 
 export default Button;
