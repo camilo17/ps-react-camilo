054ac50e432ae4187c740e7690cce126e6adf2f7
camilo17 <cami172013@gmail.com>
2018-05-30 20:16:02 -0500

    Added more example code

--- src/docs/examples/Image/ImageExample.js
+++ src/docs/examples/Image/ImageExample.js
@@ -4,14 +4,16 @@ import Box from "react-ultra-instinct/Box";
 
 export default () => {
   return (
-    <Box direction={"column"}>
+    <Box direction={"row"} justifyContent="space-between">
       <Image
         alt={"Skyline photo"}
-        src={`https://cdn.pixabay.com/photo/2018/01/27/23/46/waters-3112508_960_720.jpg`}
+        src={`https://cdn.pixabay.com/photo/2016/03/09/15/13/water-1246527_960_720.jpg`}
+        width={"100%"}
       />
       <Image
         alt={"Skyline photo"}
         src={`https://cdn.pixabay.com/photo/2018/01/27/23/46/waters-3112508_960_720.jpg`}
+        width={"100%"}
       />
     </Box>
   );
