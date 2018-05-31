8fe2d7e3810bfacab6db9295d2a27f147544b51b
camilo17 <cami172013@gmail.com>
2018-05-30 20:31:33 -0500

    Added a caption to it

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const Img = styled.figure`
+  position: relative;
   background-position: ${props => props.backgroundPosition};
   background-size: ${props => props.backgroundSize};
 
@@ -12,9 +13,18 @@ const Img = styled.figure`
   background-image: ${props => `url(${props.src})`};
 `;
 
+const ImgCaption = styled.figcaption`
+  position: absolute;
+  bottom: 0;
+  transform: translateY(20px);
+`;
+
 export default class Image extends Component {
   render() {
-    return <Img {...this.props} />;
+    const { caption, ...otherProps } = this.props;
+    return (
+      <Img {...otherProps}>{caption && <ImgCaption>{caption}</ImgCaption>}</Img>
+    );
   }
 }
 
@@ -30,5 +40,6 @@ Image.propTypes = {
   height: PropTypes.string,
   width: PropTypes.string,
   backgroundPosition: PropTypes.string,
-  backgroundSize: PropTypes.string
+  backgroundSize: PropTypes.string,
+  caption: PropTypes.string
 };
