6b41df6d3640ec48ed5ab8980e5800efcd9e1905
camilo17 <cami172013@gmail.com>
2018-06-01 15:27:47 -0500

    Created a panel component with an example

---
+++ src/components/Panel/Panel.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import is from "styled-is";
+
+const SlideContent = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background: rgba(142, 233, 232, 0.9);
+  padding: 20px;
+  color: white;
+  box-sizing: border-box;
+  transition: transform 0.3s ease;
+
+  ${is("right")`
+    transform: translate(100%, 0);
+  `};
+
+  ${is("bottom")`
+    transform: translate(0, 100%);
+  `};
+
+  ${is("top")`
+    transform: translate(0, -100%);
+  `};
+
+  ${is("left")`
+  transform: translate(-100%, 0);
+  `};
+`;
+
+const SlideImage = styled.img`
+    vertical-align: middle;
+}
+`;
+
+const Slide = styled.div`
+  position: relative;
+  display: inline-block;
+  overflow: hidden;
+
+  &:hover ${SlideContent} {
+    transform: translate(0, 0);
+  }
+`;
+export default class Panel extends Component {
+  render() {
+    const {
+      src,
+      height,
+      width,
+      right,
+      left,
+      top,
+      bottom,
+      content,
+      imageClass
+    } = this.props;
+
+    return (
+      <Slide>
+        <SlideContent right={right} left={left} top={top} bottom={bottom}>
+          {content}
+        </SlideContent>
+        <SlideImage
+          src={src}
+          height={height}
+          width={width}
+          className={imageClass}
+        />
+      </Slide>
+    );
+  }
+}
+
+Panel.propTypes = {
+  src: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  direction: PropTypes.oneOf(["right, left, top, bottom"]),
+  imageClass: PropTypes.string
+};
---
+++ src/docs/examples/Panel/PanelExample.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Panel from "react-ultra-instinct/Panel";
+
+export default () => {
+  return (
+    <Panel
+      src="https://images.unsplash.com/photo-1517894001190-8dd1d382ec85?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a056faba6740f4c998b92c21b344dfbd&auto=format&fit=crop&w=400&q=60"
+      height="400px"
+      width="400px"
+      right
+      content={"Hello world"}
+    />
+  );
+};
